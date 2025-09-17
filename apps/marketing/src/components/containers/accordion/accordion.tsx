aca312e4c06d623fe58b0d5e99b5e90cdd65cb56
Vladislav Zhonnyk <zvlad9923@gmail.com>
2025-09-18 00:23:43 +0300

    fix: accordion

--- apps/marketing/src/components/containers/accordion/accordion.tsx
+++ apps/marketing/src/components/containers/accordion/accordion.tsx
@@ -25,7 +25,6 @@ function AccordionItem({
 }
 
 function AccordionTrigger({
-  className,
   children,
   ...props
 }: React.ComponentProps<typeof AccordionPrimitive.Trigger>) {
@@ -35,7 +34,6 @@ function AccordionTrigger({
         data-slot="accordion-trigger"
         className={cn(
           "group focus-visible:border-ring focus-visible:ring-ring/50 flex flex-1 items-start justify-between gap-4 rounded-md py-4 text-left text-sm font-medium transition-all outline-none hover:underline focus-visible:ring-[3px] disabled:pointer-events-none disabled:opacity-50 [&[data-state=open]>div]:rotate-135",
-          className
         )}
         {...props}
       >
