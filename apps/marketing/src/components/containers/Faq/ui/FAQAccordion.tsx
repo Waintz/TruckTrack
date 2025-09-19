152f6ae14a3331c8e6b0dba019ab527a93390562
Vladislav Zhonnyk <zvlad9923@gmail.com>
2025-09-19 09:39:33 +0300

    feature(table) Added a table for viewing trucks

--- apps/marketing/src/components/containers/Faq/ui/FAQAccordion.tsx
+++ apps/marketing/src/components/containers/Faq/ui/FAQAccordion.tsx
@@ -11,13 +11,28 @@ interface Props {
 }
 export function FAQAccordion({ FAQ_QUESTIONS }: Props) {
   return (
-    <Accordion type="single" collapsible>
+    <Accordion data-slot="accordion" type="single" collapsible={true}>
       {FAQ_QUESTIONS.map((item, index) => (
-        <AccordionItem value={`item-${index}`} key={index}>
-          <AccordionTrigger className="font-semibold text-[15px]">
+        <AccordionItem
+          className="w-full"
+          data-slot="accordion-item"
+          value={`item-${index}`}
+          key={index}
+        >
+          <AccordionTrigger
+            data-slot="accordion-trigger"
+            className="group w-full focus-visible:border-ring focus-visible:ring-ring/50 flex justify-between gap-4 rounded-md py-4 text-left text-sm transition-all outline-none hover:underline focus-visible:ring-[3px] disabled:pointer-events-none disabled:opacity-50 [&[data-state=open]>div]:rotate-135 font-semibold text-[15px]"
+          >
             {item.question}
           </AccordionTrigger>
-          <AccordionContent>{item.answer}</AccordionContent>
+          <AccordionContent
+            data-slot="accordion-content"
+            className="data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down overflow-hidden text-sm"
+          >
+            <div className="pt-0 pb-4">
+              {item.answer}
+            </div>
+          </AccordionContent>
         </AccordionItem>
       ))}
     </Accordion>
